73780b85a112771530147abd90be1d0857d4b111
frankkuipers <frankkuipers@outlook.com>
2018-05-08 22:43:38 +0200

    Added SOLOpenIdUser class

--- src/SOLOpenIdClient.php
+++ src/SOLOpenIdClient.php
@@ -13,6 +13,8 @@ namespace ScoutingRudyardKipling;
  * Please visit the documentation by SNL on their OpenId server for more information;
  * https://www.scouting.nl/downloads/ondersteuning/internet/2778-open-id-handleiding-voor-webmasters/file
  *
+ * @property-read string $mode
+ * @property string      $identity
  * @package ScoutingRudyardKipling
  */
 class SOLOpenIdClient extends \LightOpenID
@@ -22,7 +24,8 @@ class SOLOpenIdClient extends \LightOpenID
     const THEME_EARTH = 'TC3_earth';
     const THEME_WATER = 'TC3_water';
 
-    private $theme = self::THEME_LEAF;
+    private        $theme               = self::THEME_LEAF;
+    private static $snl_login_namespace = 'https://login.scouting.nl/user/';
 
     /**
      * Set the theme of the login page theme
@@ -32,6 +35,41 @@ class SOLOpenIdClient extends \LightOpenID
     public function setTheme($theme = self::THEME_LEAF)
     {
         $this->theme = $theme;
+    }
+
+    /**
+     * Set the user identity
+     *
+     * @param $username
+     * @return $this
+     */
+    public function setUserIdentity($username)
+    {
+        $namespace = explode('/', $username);
+        $username  = array_pop($namespace);
+
+        $this->identity = self::$snl_login_namespace . strtolower($username);
+
+        return $this;
+    }
+
+    /**
+     * @return SOLOpenIdUser
+     */
+    public function getValidatedUser()
+    {
+        $instance = new SOLOpenIdUser();
+
+        /** @var string[] $attributes */
+        $attributes = $this->getSregAttributes();
+
+        $instance->fullName          = $attributes['namePerson'];
+        $instance->email             = $attributes['contact/email'];
+        $instance->birthDate         = $attributes['birthDate'];
+        $instance->gender            = $attributes['person/gender'];
+        $instance->preferredLanguage = $attributes['pref/language'];
+
+        return $instance;
     }
 
     /**
---
+++ src/SOLOpenIdUser.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by: Frank Kuipers
+ * Date: 8-5-18
+ * Time: 22:27
+ */
+
+namespace ScoutingRudyardKipling;
+
+class SOLOpenIdUser
+{
+
+    /**
+     * @var string
+     */
+    public $fullName;
+
+    /**
+     * Gender, either M of F
+     *
+     * @var string
+     */
+    public $gender;
+
+    /**
+     * @var string
+     */
+    public $email;
+
+    /**
+     * @var string
+     */
+    public $birthDate;
+
+    /**
+     * @var string
+     */
+    public $preferredLanguage;
+}
